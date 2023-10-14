6ba502647de57a6754bd0b8e9780bab650814b67
Deniz Demir <125642528+deniz-9@users.noreply.github.com>
2023-10-14 09:20:25 +0300

    revize edildi.

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,137 +1,178 @@
-
-
+import axios from "axios";
 import React, { useState } from "react";
 
+const initialState = {
+  message: "",
+  email: "",
+  steps: 0,
+  index: 4,
+};
+
 export default function AppFunctional(props) {
-  const [location, setLocation] = useState([2, 2]);
-  const [message, setMessage] = useState("");
-  const [step, setStep] = useState(0);
-  const [form, setForm] = useState("");
+  const [state, setState] = useState(initialState);
 
-  
+  function getXY() {
+    let x = (state.index % 3) + 1;
+    let y = Math.floor(state.index / 3) + 1;
 
-  function onChange(event) {
-   
-    setForm(event.target.value);
+    return { x, y };
   }
 
-  function onSubmit(event) {
-    event.preventDefault();
-    let sonuc = {
-      x: location[0],
-      y: location[1],
-      steps: step,
-      email: form,
-    };
-    axios
-      .post("http://localhost:9000/api/result", sonuc)
-      .then((res) => {
-        console.log(res.data);
-        reset();
-      })
-      .catch(() => {
-        console.log("Unprocessable Entity");
-      });
-  }
-
-  
-
-  function up() {
-    setMessage("");
-
-    if (location[1] > 1) {
-      setLocation([location[0], location[1] - 1]);
-      setStep(step + 1);
-    } else {
-      setMessage("YUKARI GİDEMEZSİNİZ");
-    }
-  }
-
-  function right() {
-    setMessage("");
-    // x değişecek , y sabit
-    if (location[0] < 3) {
-      setLocation([location[0] + 1, location[1]]);
-      setStep(step + 1);
-    } else {
-      setMessage("SAĞA GİDEMEZSİNİZ");
-    }
-  }
-
-  function down() {
-    setMessage("");
-
-    if (location[1] < 3) {
-      setLocation([location[0], location[1] + 1]);
-      setStep(step + 1);
-    } else {
-      setMessage("AŞAĞI GİDEMEZSİNİZ");
-    }
-  }
-
-  function left() {
-    setMessage("");
-
-    if (location[0] > 1) {
-      setLocation([location[0] - 1, location[1]]);
-      setStep(step + 1);
-    } else {
-      setMessage("SOLA GİDEMEZSİNİZ");
-    }
+  function getXYMesaj() {
+    return `Koordinatlar (${getXY().x}, ${getXY().y})`;
   }
 
   function reset() {
-    setLocation([2, 2]);
-    setStep(0);
-    setMessage("");
+    setState(initialState);
   }
 
+  function sonrakiIndex(yon) {
+    let x = getXY().x;
+    let y = getXY().y;
 
-  const initialIndex = (location[1] - 1) * 3 + location[0] - 1;
+    switch (yon) {
+      case "left":
+        x > 1 &&
+          setState({
+            ...state,
+            index: state.index - 1,
+            steps: state.steps + 1,
+            message: "",
+          });
+        break;
+      case "right":
+        x < 3 &&
+          setState({
+            ...state,
+            index: state.index + 1,
+            steps: state.steps + 1,
+            message: "",
+          });
+        break;
+      case "up":
+        y > 1 &&
+          setState({
+            ...state,
+            index: state.index - 3,
+            steps: state.steps + 1,
+            message: "",
+          });
+        break;
+      case "down":
+        y < 3 &&
+          setState({
+            ...state,
+            index: state.index + 3,
+            steps: state.steps + 1,
+            message: "",
+          });
+        break;
+      default:
+        break;
+    }
+  }
+
+  function ilerle(evt) {
+    const { id: yon } = evt.target;
+
+    if (getXY().x === 1 && yon === "left") {
+      setState({
+        ...state,
+        message: "Sola gidemezsiniz",
+      });
+    } else if (getXY().x === 3 && yon === "right") {
+      setState({
+        ...state,
+        message: "Sağa gidemezsiniz",
+      });
+    } else if (getXY().y === 1 && yon === "up") {
+      setState({
+        ...state,
+        message: "Yukarı gidemezsiniz",
+      });
+    } else if (getXY().y === 3 && yon === "down") {
+      setState({
+        ...state,
+        message: "Aşağıya gidemezsiniz",
+      });
+    } else {
+      sonrakiIndex(yon);
+    }
+  }
+
+  function onChange(evt) {
+    const { name, value } = evt.target;
+
+    setState({
+      ...state,
+      [name]: value,
+    });
+  }
+
+  function onSubmit(evt) {
+    evt.preventDefault();
+    axios
+      .post("http://localhost:9000/api/result", {
+        x: getXY().x,
+        y: getXY().y,
+        email: state.email,
+        steps: state.steps,
+      })
+      .then((response) => {
+        setState({
+          ...state,
+          message: response.data.message,
+          email: "",
+        });
+      });
+  }
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar ({location.join(", ")})</h3>
-        <h3 id="steps">{step} kere ilerlediniz</h3>
+        <h3 id="coordinates">{getXYMesaj()}</h3>
+        <h3 id="steps">{state.steps} kere ilerlediniz</h3>
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
           <div
             key={idx}
-            className={`square${idx === initialIndex ? " active" : ""}`}
+            className={`square${idx === state.index ? " active" : ""}`}
           >
-            {idx === initialIndex ? "B" : null}
+            {idx === state.index ? "B" : null}
           </div>
         ))}
       </div>
       <div className="info">
-        <h3 id="message">{message}</h3>
+        <h3 id="message">{state.message}</h3>
       </div>
       <div id="keypad">
-        <button id="left" onClick={left}>
+        <button id="left" onClick={ilerle}>
           SOL
         </button>
-        <button id="up" onClick={up}>
+        <button id="up" onClick={ilerle}>
           YUKARI
         </button>
-        <button id="right" onClick={right}>
+        <button id="right" onClick={ilerle}>
           SAĞ
         </button>
-        <button id="down" onClick={down}>
+        <button id="down" onClick={ilerle}>
           AŞAĞI
         </button>
         <button id="reset" onClick={reset}>
-          RESET
+          reset
         </button>
       </div>
       <form onSubmit={onSubmit}>
         <input
           id="email"
           type="email"
-          placeholder="Emailinizi girin"
+          name="email"
           onChange={onChange}
-        ></input>
-        <input id="submit" type="submit"></input>
+          value={state.email}
+          placeholder="email girin"
+        />
+        <input id="submit" type="submit" value="Gönder" />
       </form>
     </div>
   );
